e58ceb919324292b63545983bdb43428c4359898
Masaya Kataoka <ms.kataoka@gmail.com>
2021-07-07 22:51:21 +0900

    add copyright notice
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- playstation_controller_drivers/src/dualsense_driver_component.cpp
+++ playstation_controller_drivers/src/dualsense_driver_component.cpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2021 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <playstation_controller_drivers/dualsense_driver_component.hpp>
 #include <playstation_controller_drivers/util.hpp>
 #include <color_names/color_names.hpp>
@@ -133,6 +147,8 @@ void DualsenseDriverComponent::timerCallback()
       axis_[SDL_CONTROLLER_AXIS_LEFTY] = 0;
       axis_[SDL_CONTROLLER_AXIS_RIGHTX] = 0;
       axis_[SDL_CONTROLLER_AXIS_RIGHTY] = 0;
+      axis_[SDL_CONTROLLER_AXIS_TRIGGERLEFT] = 0;
+      axis_[SDL_CONTROLLER_AXIS_TRIGGERRIGHT] = 0;
     }
   } else {
     sensor_msgs::msg::Joy joy;
@@ -204,6 +220,12 @@ void DualsenseDriverComponent::timerCallback()
           break;
         case SDL_CONTROLLER_AXIS_RIGHTY:
           joy.axes[3] = axis.second;
+          break;
+        case SDL_CONTROLLER_AXIS_TRIGGERLEFT:
+          joy.axes[4] = axis.second * -1;
+          break;
+        case SDL_CONTROLLER_AXIS_TRIGGERRIGHT:
+          joy.axes[5] = axis.second * -1;
           break;
         default:
           break;
